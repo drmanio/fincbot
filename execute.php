52c130b289ace35f402448417a25fd6a58691c45
drmanio <46829653+drmanio@users.noreply.github.com>
2019-01-19 00:16:50 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,29 +1,28 @@
 <?php
-      // recupero il contenuto inviato da Telegram
-      $content = file_get_contents("php://input");
-      // converto il contenuto da JSON ad array PHP
-      $update = json_decode($content, true);
-      // se la richiesta è null interrompo lo script
-      if(!$update)
-      {
-        exit;
-      }
-      // assegno alle seguenti variabili il contenuto ricevuto da Telegram
-      $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-      $url = "https://api.fattureincloud.it/v1/richiesta/info";
-      $request = array("api_uid" => "295254", "api_key" => "58a05f9b8bd43c554f97a3731a1ddb6e");
-      $options = array(
-          "http" => array(
-              "header"  => "Content-type: text/json\r\n",
-              "method"  => "POST",
-              "content" => json_encode($request)
-          ),
-      );
-      $context  = stream_context_create($options);
-      $result = json_decode(file_get_contents($url, false, $context), true);
-      $text = $result["messaggio"];
+// recupero il contenuto inviato da Telegram
+$content = file_get_contents("php://input");
+// converto il contenuto da JSON ad array PHP
+$update = json_decode($content, true);
+// se la richiesta è null interrompo lo script
+if(!$update)
+{
+  exit;
+}
+// assegno alle seguenti variabili il contenuto ricevuto da Telegram
+$message = isset($update['message']) ? $update['message'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
+$date = isset($message['date']) ? $message['date'] : "";
+$text = isset($message['text']) ? $message['text'] : "";
+// pulisco il messaggio ricevuto togliendo eventuali spazi prima e dopo il testo
+$text = trim($text);
+// converto tutti i caratteri alfanumerici del messaggio in minuscolo
+$text = strtolower($text);
 // mi preparo a restitutire al chiamante la mia risposta che è un oggetto JSON
-// imposto l'header della risposta     
+// imposto l'header della risposta
 header("Content-Type: application/json");
 // la mia risposta è un array JSON composto da chat_id, text, method
 // chat_id mi consente di rispondere allo specifico utente che ha scritto al bot
