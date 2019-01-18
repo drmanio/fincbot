48ab746db5bb7e80db33186d2e94732076911c86
drmanio <46829653+drmanio@users.noreply.github.com>
2019-01-19 00:02:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo "Silence is golden";
+<?php
+      $url = "https://api.fattureincloud.it/v1/richiesta/info";
+      $request = array("api_uid" => "295254", "api_key" => "58a05f9b8bd43c554f97a3731a1ddb6e");
+      $options = array(
+          "http" => array(
+              "header"  => "Content-type: text/json\r\n",
+              "method"  => "POST",
+              "content" => json_encode($request)
+          ),
+      );
+      $context  = stream_context_create($options);
+      $result = json_decode(file_get_contents($url, false, $context), true);
+      print_r($result);
+?>
