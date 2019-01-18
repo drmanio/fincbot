95e2273052f6e4cbfe454d2d7f09406dd489a163
drmanio <46829653+drmanio@users.noreply.github.com>
2019-01-19 00:09:31 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,4 +1,15 @@
 <?php
+      // recupero il contenuto inviato da Telegram
+      $content = file_get_contents("php://input");
+      // converto il contenuto da JSON ad array PHP
+      $update = json_decode($content, true);
+      // se la richiesta è null interrompo lo script
+      if(!$update)
+      {
+        exit;
+      }
+      // assegno alle seguenti variabili il contenuto ricevuto da Telegram
+      $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
       $url = "https://api.fattureincloud.it/v1/richiesta/info";
       $request = array("api_uid" => "295254", "api_key" => "58a05f9b8bd43c554f97a3731a1ddb6e");
       $options = array(
@@ -10,4 +21,15 @@
       );
       $context  = stream_context_create($options);
       $result = json_decode(file_get_contents($url, false, $context), true);
-      print_r($result);
+      $text($result["messaggio"]);
+// mi preparo a restitutire al chiamante la mia risposta che è un oggetto JSON
+// imposto l'header della risposta     
+header("Content-Type: application/json");
+// la mia risposta è un array JSON composto da chat_id, text, method
+// chat_id mi consente di rispondere allo specifico utente che ha scritto al bot
+// text è il testo della risposta
+$parameters = array('chat_id' => $chatId, "text" => $text);
+// method è il metodo per l'invio di un messaggio (cfr. API di Telegram)
+$parameters["method"] = "sendMessage";
+// converto e stampo l'array JSON sulla response
+echo json_encode($parameters);
