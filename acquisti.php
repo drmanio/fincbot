f3037261216ca39a7b542506b9c0e335bc9950ad
drmanio <46829653+drmanio@users.noreply.github.com>
2019-01-22 23:37:06 +0100

    Update acquisti.php

--- acquisti.php
+++ acquisti.php
@@ -1,6 +1,6 @@
 <?php
 function acquisti($text) {
-	if($text=="/acquisti") {
+
 		$url = "https://api.fattureincloud.it/v1/acquisti/lista";
 		$request = array("api_uid" => "295254", "api_key" => "58a05f9b8bd43c554f97a3731a1ddb6e", "anno" => 2018);
 		$options = array(
@@ -13,13 +13,7 @@ function acquisti($text) {
 		$context  = stream_context_create($options);
 		$result = json_decode(file_get_contents($url, false, $context), true);
 		
-		if($text=="/acquisti") {
-			$responsef = $result["lista_documenti"]["0"]["nome"];
-		}
-	
-	}
-	else {
-		$responsef = "Interrogazione non eseguita. Richiesta errata!";
-	}
-	return $responsef;
+		$responsef = $result["lista_documenti"];
+
+return $responsef;
 }
