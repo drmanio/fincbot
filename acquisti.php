39fc1d1f449ac33a7789f835d828b06d61e43b06
drmanio <46829653+drmanio@users.noreply.github.com>
2019-01-22 23:14:13 +0100

    Update and rename acquisti to acquisti.php

---
+++ acquisti.php
@@ -0,0 +1,25 @@
+<?php
+function acquisti($text) {
+	if($text=="/acquisti") {
+		$url = "https://api.fattureincloud.it/v1/acquisti/lista";
+		$request = array("api_uid" => "295254", "api_key" => "58a05f9b8bd43c554f97a3731a1ddb6e", "anno" => 2018);
+		$options = array(
+		"http" => array(
+		"header"  => "Content-type: text/json\r\n",
+		"method"  => "POST",
+		"content" => json_encode($request)
+		),
+		);
+		$context  = stream_context_create($options);
+		$result = json_decode(file_get_contents($url, false, $context), true);
+		
+		if($text=="/acquisti") {
+			$responsef = $result["lista_documenti"];
+		}
+	
+	}
+	else {
+		$responsef = "Interrogazione non eseguita. Richiesta errata!";
+	}
+	return $responsef;
+}
