c1c4fc67f50095f7aac4272fad9ed0384a057b1c
drmanio <46829653+drmanio@users.noreply.github.com>
2019-01-23 23:52:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+      $annocorrente = date("Y");
+      echo $annocorrente;
+      
+      
       $url = "https://api.fattureincloud.it/v1/acquisti/lista";
-      $request = array("api_uid" => "295254", "api_key" => "58a05f9b8bd43c554f97a3731a1ddb6e", "anno"=> ">2018");
+      $request = array("api_uid" => "295254", "api_key" => "58a05f9b8bd43c554f97a3731a1ddb6e", "anno"=> 2018);
       $options = array(
           "http" => array(
               "header"  => "Content-type: text/json\r\n",
