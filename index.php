68714ef0236991eab5ecc7f53dcfb57e6a0c1594
drmanio <46829653+drmanio@users.noreply.github.com>
2019-01-23 23:59:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,23 @@
 <?php
-      $annocorrente = date("Y");
-      echo $annocorrente;
       
+$result = array();
+      
+for($i = 2018; $i <= date("Y"); $i++) {
       
       $url = "https://api.fattureincloud.it/v1/acquisti/lista";
       $request = array("api_uid" => "295254", "api_key" => "58a05f9b8bd43c554f97a3731a1ddb6e", "anno"=> 2018);
       $options = array(
-          "http" => array(
-              "header"  => "Content-type: text/json\r\n",
-              "method"  => "POST",
-              "content" => json_encode($request)
-          ),
-      );
+            "http" => array(
+                  "header"  => "Content-type: text/json\r\n",
+                  "method"  => "POST",
+                  "content" => json_encode($request)
+                  ),
+            );
       $context  = stream_context_create($options);
-      $result2018 = json_decode(file_get_contents($url, false, $context), true);
+      $result[] = json_decode(file_get_contents($url, false, $context), true);
       
-      $url = "https://api.fattureincloud.it/v1/acquisti/lista";
-      $request = array("api_uid" => "295254", "api_key" => "58a05f9b8bd43c554f97a3731a1ddb6e", "anno"=> 2019);
-      $options = array(
-          "http" => array(
-              "header"  => "Content-type: text/json\r\n",
-              "method"  => "POST",
-              "content" => json_encode($request)
-          ),
-      );
-      $context  = stream_context_create($options);
-      $result2019 = json_decode(file_get_contents($url, false, $context), true);
+}
 
-      $result = array();
-      $result[] = $result2018[lista_documenti];
-      $result[] = $result2019[lista_documenti];
+print_r($result);
 
-      print_r($result);
 ?>
