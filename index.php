58f9639bb5e6c0f7c2a06250ae9a55764a6a97be
drmanio <46829653+drmanio@users.noreply.github.com>
2019-01-23 23:38:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,23 @@
           ),
       );
       $context  = stream_context_create($options);
-      $result = json_decode(file_get_contents($url, false, $context), true);
+      $result2018 = json_decode(file_get_contents($url, false, $context), true);
+      
+      $url = "https://api.fattureincloud.it/v1/acquisti/lista";
+      $request = array("api_uid" => "295254", "api_key" => "58a05f9b8bd43c554f97a3731a1ddb6e", "anno"=> 2019);
+      $options = array(
+          "http" => array(
+              "header"  => "Content-type: text/json\r\n",
+              "method"  => "POST",
+              "content" => json_encode($request)
+          ),
+      );
+      $context  = stream_context_create($options);
+      $result2019 = json_decode(file_get_contents($url, false, $context), true);
+
+      $result = array();
+      $result[] = $result2018;
+      $result[] = $result2019;
+
       print_r($result);
 ?>
